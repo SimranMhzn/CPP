15ae801b9651dfe5b6b49c012eecf25243df4e60
SimranMaharjan <smhzn2789@gmail.com>
2023-12-23 16:36:46 +0545

    type conversion done

---
+++ revised/typeconversion_!.cpp
@@ -0,0 +1,12 @@
+//implicit to add int and float
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=10,sum=0;
+	float b=2.5;
+	sum=a+b;
+	cout<<endl<<"The sum of "<<a<<" and "<<b<<" is "<<sum<<".";
+	return 0;
+}
---
+++ revised/typeconversion_2.cpp
@@ -0,0 +1,13 @@
+//explicit to convert float to int
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float num1=23.65;
+	int num2;
+	num2=(int)num1;
+	cout<<endl<<"The number before conversion is "<<num1;
+	cout<<endl<<"The number after conversion is "<<num2;
+	return 0;
+}
