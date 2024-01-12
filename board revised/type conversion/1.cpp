60a46d3e7846ccbcfed02de73827c1fc75730def
SimranMaharjan <smhzn2789@gmail.com>
2024-01-12 22:21:21 +0545

    type conversion

---
+++ board revised/type conversion/1.cpp
@@ -0,0 +1,12 @@
+//implicit type conversion
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=0;
+	float y=5.5;
+	x=y;
+	cout<<"Through implicit conversion: "<<endl;
+	cout<<"The value of x is "<<x;
+	return 0;
+}
